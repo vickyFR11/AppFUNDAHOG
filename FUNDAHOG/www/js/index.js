f043b06bcefbb04dcc546e750a145d7cdef9cc2b
vickyFR11 <victoriafr11@gmail.com>
2015-07-26 11:25:56 -0430

    Version base

--- FUNDAHOG/www/js/index.js
+++ FUNDAHOG/www/js/index.js
@@ -1,22 +1,17 @@
+function show(){
+    var sidebar = document.getElementById('barraLateral');
+    
+    if (sidebar.style.display == "none"){
+        sidebar.style.display = "block";
+    }else{
+        sidebar.style.display = "none";
+    }
+    
+}
+
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
+ * 
+ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -49,3 +44,5 @@ var app = {
 };
 
 app.initialize();
+
+*/
